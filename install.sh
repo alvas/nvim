d83a24b50b40abc1b8b65e1b94d683e7d2bbd95b
alvas <qingyun@LAPTOP-VRND5SU9.localdomain>
2020-12-24 13:26:39 -0800

    install cmake cscope ctags

--- install.sh
+++ install.sh
@@ -6,7 +6,11 @@
 # Distributed under terms of the MIT license.
 #
 
-sudo apt install neovim -y
+apt update
+apt install neovim -y
+sudo apt install cmake -y
+sudo apt install exuberant-ctags -y
+sudo apt install cscope -y
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 git clone https://github.com/alvas/nvim.git ~/.config/nvim/
 nvim +PluginInstall +qall
