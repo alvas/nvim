bbd7087bc717e919a202ea2123f8d1b591db6bc9
alvas <alvasli@LAPTOP-VRND5SU9.localdomain>
2021-04-10 14:31:54 -0700

    remove sudo install.sh

--- install.sh
+++ install.sh
@@ -8,9 +8,9 @@
 
 apt update
 apt install neovim -y
-sudo apt install cmake -y
-sudo apt install exuberant-ctags -y
-sudo apt install cscope -y
+apt install cmake -y
+apt install exuberant-ctags -y
+apt install cscope -y
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 git clone https://github.com/alvas/nvim.git ~/.config/nvim/
 nvim +PluginInstall +qall
